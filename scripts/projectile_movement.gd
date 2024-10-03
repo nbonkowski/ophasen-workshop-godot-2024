d3242885fff92911368e02b50c4cf07d8b51d682
Niklas <31795243+nbonkowski@users.noreply.github.com>
2024-10-03 22:09:47 +0200

    add movement to projectile

--- scripts/projectile_movement.gd
+++ scripts/projectile_movement.gd
@@ -1,12 +1,20 @@
 extends CharacterBody2D
 
+@export var projectile_speed = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	velocity = -transform.y * 400
-
+	velocity = -transform.y * projectile_speed 
+	$Timer.stop
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	move_and_slide()
+		
+func explode()-> void:
+	queue_free()
 	
+
+
+func _on_timer_timeout() -> void:
+	explode() # Replace with function body.
