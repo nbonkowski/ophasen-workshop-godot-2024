ed91b0ffc4dbb47d43b33aa1a8072f8b01ba5389
Niklas <31795243+nbonkowski@users.noreply.github.com>
2024-10-04 18:10:31 +0200

    changed to tile layer and changed projectile

--- scripts/projectile_movement.gd
+++ scripts/projectile_movement.gd
@@ -1,20 +1,25 @@
-extends CharacterBody2D
+extends Area2D
 
-@export var projectile_speed = 500
+@export var projectile_speed = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	velocity = -transform.y * projectile_speed 
+	#velocity = -transform.y * projectile_speed 
 	$Timer.stop
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	move_and_slide()
+	position += -transform.y * projectile_speed * delta
+	#move_and_slide()
 		
 func explode()-> void:
 	queue_free()
-	
+	queue_free()
 
 
 func _on_timer_timeout() -> void:
 	explode() # Replace with function body.
+
+
+func _on_body_entered(body: Node2D) -> void:
+	explode()
